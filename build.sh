58f23576bdda4cfaccef7ddf0aa9a509890e4755
hxfuxyy <viitiv.mmm@gmail.com>
2025-01-11 14:43:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,33 +6,43 @@ rm -rf .repo/local_manifests
 echo "----------------DELETED DIRECTORIES----------------"
 
 #Initialise repos
-repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
+repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
 echo "--------------REPO INITIALISED---------------"
-
-#Local Manifest
-git clone https://github.com/hxfuxyy/nabu-local_manifest --depth 1 -b rising-v .repo/local_manifests
-echo "-----------------CLONED local manifest-------------------"
 
 #Resync
 /opt/crave/resync.sh
 echo "---------------RESYNCED-----------------"
+
+rm -rf packages/apps/Settings
+rm -rf device/xiaomi/nabu
+rm -rf vendor/xiaomi/nabu
+rm -rf kernel/xiaomi/nabu
+rm -rf hardware/xiaomi
+echo "----------------DELETED DIRECTORIES----------------"
+
+git clone -b 15.0 https://github.com/hxfuxyy/android_packages_apps_Settings packages/apps/Settings
+git clone -b crdroid-v https://github.com/hxfuxyy/android_device_xiaomi_nabu device/xiaomi/nabu
+git clone -b lineage-22 https://github.com/dev-harsh1998/android_kernel_xiaomi_nabu kernel/xiaomi/nabu
+git clone -b lineage-22 https://gitlab.com/dev-harsh1998/android_vendor_xiaomi_nabu vendor/xiaomi/nabu
+git clone -b 15.0 https://github.com/crdroidandroid/android_hardware_xiaomi hardware/xiaomi
+echo "---------------RESYNCED-----------------"
+
+# Upgrade System and install openssl
+sudo apt update && sudo apt upgrade -y
+sudo apt update && sudo apt install libc6-dev
+sudo apt install openssl libssl-dev -y && sudo apt install libssl-dev -y
+sudo apt-get install libfl-dev -y
+
+echo "-----------------SYSTEM UPGRADED-----------------"
+
 #Build Environment
 . build/envsetup.sh
 echo "---------------BUILD ENVIRONMENT------------------"
 
-#Lunch
-riseup nabu userdebug
-echo "--------------VANILLA BUILD STARTED--------------"
-rise b
+#Brunch
+echo "--------------CRDROID BUILD STARTED--------------"
+brunch nabu
 
-mv out/target/product/nabu/*.zip .
+cp out/target/product/nabu/*.zip .
 echo "--------------MOVED VANILLA ZIPS TO ROOT DIRECTORY--------------"
-
-#Exports for Gapps Build
-export WITH_GMS=true
-export TARGET_CORE_GMS=true
-echo "--------------GAPPS BUILD STARTED--------------"
-rise b
-
-mv out/target/product/nabu/*.zip .
-echo "--------------MOVED GAPPS ZIPS TO ROOT DIRECTORY----------------"
+echo "--------------BUILD SUCCESSFULLY--------------"
